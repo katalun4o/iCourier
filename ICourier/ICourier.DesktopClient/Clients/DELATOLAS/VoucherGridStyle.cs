37041358a113c52786c4454600cf4a330faf5e1f
katalun4o <katalun4o@gmail.com>
2016-05-25 21:17:21 +0300

    added column style loading for delatolas voucher search

--- ICourier/ICourier.DesktopClient/Clients/DELATOLAS/VoucherGridStyle.cs
+++ ICourier/ICourier.DesktopClient/Clients/DELATOLAS/VoucherGridStyle.cs
@@ -18,50 +18,54 @@ namespace LightSwitchApplication.Clients.DELATOLAS
         public static void CreateStyle(ObservableCollection<DataGridColumn> columns)
         {
             System.Collections.Generic.Dictionary<string, GridColumnInfo> fieldNames = new System.Collections.Generic.Dictionary<string, GridColumnInfo>();
-            fieldNames.Add("Number", new GridColumnInfo("ΑΡΙΘΜΟΣ", true, 1));
-            fieldNames.Add("Volume", new GridColumnInfo("Κυβικά", true, 2));
-            fieldNames.Add("PayWayCash", new GridColumnInfo("Τρόπος Αντικαταβολής - Μετρητά", true, 3));
-            fieldNames.Add("PayWayCheck", new GridColumnInfo("Τρόπος Αντικαταβολής - Επιταγή", true, 4));
-            fieldNames.Add("PayWayBillOfExchange", new GridColumnInfo("Τρόπος Αντικαταβολής - Συναλαγματική", true, 5));
-            fieldNames.Add("PayWayDesc", new GridColumnInfo("Σχόλια Αντικαταβολής", true, 6));
-            fieldNames.Add("DeliveryNoteInvoice", new GridColumnInfo("Δελτίο Αποστολής - Τιμολόγιο", true, 7));
-            fieldNames.Add("VoucherGroup", new GridColumnInfo("Ομάδα", true, 8));
-            fieldNames.Add("PackType", new GridColumnInfo("Είδος συσκευασίας", true, 9));
-            fieldNames.Add("Invoice", new GridColumnInfo("Παραστατικό", true, 10));
-            fieldNames.Add("MobilePhone", new GridColumnInfo("Κινητό τηλέφωνο", true, 11));
-            fieldNames.Add("PayWay", new GridColumnInfo("Payment Way", true, 12));
-            fieldNames.Add("CurrentCustomer", new GridColumnInfo("ΑΝΑΖ.ΠΕΛΑΤΕΣ", true, 13));            
-            fieldNames.Add("ReceiverCode", new GridColumnInfo("ΚΩΔΙΚΟΣ", true, 14));
-            fieldNames.Add("ReceiverName", new GridColumnInfo("ΠΑΡΑΛΗΠΤΗΣ", true, 15));
-            fieldNames.Add("ReceiverAddress", new GridColumnInfo("Δ/ΝΣΗ", true, 16));
-            fieldNames.Add("ReceiverStreetNum", new GridColumnInfo("ΑΡ.", true, 17));
-            fieldNames.Add("ReceiverCity", new GridColumnInfo("ΝΟΜΟΣ", true, 18));
-            fieldNames.Add("ReceiverArea", new GridColumnInfo("ΠΕΡΙΟΧΗ", true, 19));
-            fieldNames.Add("ReceiverPK", new GridColumnInfo("TK", true, 20));
-            fieldNames.Add("ReceiverTaxNum", new GridColumnInfo("AΦΜ", true, 20));
-            fieldNames.Add("ReceiverPhone", new GridColumnInfo("ΤΗΛ.", true, 21));
-            fieldNames.Add("ReceiverPhone1", new GridColumnInfo("ΤΗΛ.1", false, 22));
-            fieldNames.Add("Antikatavoli", new GridColumnInfo("ΑΝΤ/ΛΗ", true, 23));
-            fieldNames.Add("CashCheck", new GridColumnInfo("ΤΡ. ΠΛ.", false, 24));
-            fieldNames.Add("Memo", new GridColumnInfo("ΣΧΟΛΙΑ", true, 25));
-            fieldNames.Add("DeliveryHourFrom", new GridColumnInfo("ΩΡΑ ΑΠΟ", true, 26));
-            fieldNames.Add("DeliveryHourTo", new GridColumnInfo("ΩΡΑ ΕΩΣ", true, 27));
-            fieldNames.Add("PayerType", new GridColumnInfo("ΧΡΕΩΣΗ", true, 28));
-            fieldNames.Add("PackageType", new GridColumnInfo("ΔΕΜΑ/ΦΑΚΕΛΟΣ", false, 29));
-            fieldNames.Add("IsReturn", new GridColumnInfo("ΕΠΙΣΤΡΟΦΗ", true, 30));
-            fieldNames.Add("IsSaturday", new GridColumnInfo("ΣΑΒΒΑΤΟ", true, 31));
-            fieldNames.Add("CostCenter", new GridColumnInfo("ΚΕΝΤΡΟ ΚΟΣΤΟΥΣ", true, 32));
-            fieldNames.Add("Weight", new GridColumnInfo("ΒΑΡΟΣ", true, 33));
-            fieldNames.Add("Count", new GridColumnInfo("ΤΕΜΑΧΙΑ", true, 34));
-            fieldNames.Add("c_Date", new GridColumnInfo("ΗΜ/ΝΙΑ", true, 35));
-            fieldNames.Add("IsPrinted", new GridColumnInfo("ΕΚΤΥΠΩΣΗΣ", true, 36));
-            fieldNames.Add("BillOfLading", new GridColumnInfo("Φορτωτική", true, 37));
-            fieldNames.Add("Memo1", new GridColumnInfo("Σχόλια 1", true, 38));
-            fieldNames.Add("Memo2", new GridColumnInfo("Σχόλια 2", true, 39));
-            fieldNames.Add("TimeCommitment", new GridColumnInfo("Δεσμεύση ώρας", true, 40));
+            fieldNames.Add("Number", new GridColumnInfo("ΑΡΙΘΜΟΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("Volume", new GridColumnInfo("Κυβικά", true, fieldNames.Count + 1));
+            fieldNames.Add("PayWayCash", new GridColumnInfo("Τρόπος Αντικαταβολής - Μετρητά", true, fieldNames.Count + 1));
+            fieldNames.Add("PayWayCheck", new GridColumnInfo("Τρόπος Αντικαταβολής - Επιταγή", true, fieldNames.Count + 1));
+            fieldNames.Add("PayWayBillOfExchange", new GridColumnInfo("Τρόπος Αντικαταβολής - Συναλαγματική", true, fieldNames.Count + 1));
+            fieldNames.Add("PayWayDesc", new GridColumnInfo("Σχόλια Αντικαταβολής", true, fieldNames.Count + 1));
+            fieldNames.Add("DeliveryNoteInvoice", new GridColumnInfo("Δελτίο Αποστολής - Τιμολόγιο", true, fieldNames.Count + 1));
+            fieldNames.Add("VoucherGroup", new GridColumnInfo("Ομάδα", true, fieldNames.Count + 1));
+            fieldNames.Add("PackType", new GridColumnInfo("Είδος συσκευασίας", true, fieldNames.Count + 1));
+            fieldNames.Add("Invoice", new GridColumnInfo("Παραστατικό", true, fieldNames.Count + 1));
+            fieldNames.Add("MobilePhone", new GridColumnInfo("Κινητό τηλέφωνο", true, fieldNames.Count + 1));
+            fieldNames.Add("PayWay", new GridColumnInfo("Payment Way", true, fieldNames.Count + 1));
+            fieldNames.Add("CurrentCustomer", new GridColumnInfo("ΑΝΑΖ.ΠΕΛΑΤΕΣ", true, fieldNames.Count + 1));            
+            fieldNames.Add("ReceiverCode", new GridColumnInfo("ΚΩΔΙΚΟΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverName", new GridColumnInfo("ΠΑΡΑΛΗΠΤΗΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverAddress", new GridColumnInfo("Δ/ΝΣΗ", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverStreetNum", new GridColumnInfo("ΑΡ.", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverCity", new GridColumnInfo("ΝΟΜΟΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverArea", new GridColumnInfo("ΠΕΡΙΟΧΗ", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverPK", new GridColumnInfo("TK", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverTaxNum", new GridColumnInfo("AΦΜ", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverPhone", new GridColumnInfo("ΤΗΛ.", true, fieldNames.Count + 1));
+            fieldNames.Add("ReceiverPhone1", new GridColumnInfo("ΤΗΛ.1", false, fieldNames.Count + 1));
+            fieldNames.Add("Antikatavoli", new GridColumnInfo("ΑΝΤ/ΛΗ", true, fieldNames.Count + 1));
+            fieldNames.Add("CashCheck", new GridColumnInfo("ΤΡ. ΠΛ.", false, fieldNames.Count + 1));
+            fieldNames.Add("Memo", new GridColumnInfo("ΣΧΟΛΙΑ", true, fieldNames.Count + 1));
+            fieldNames.Add("DeliveryHourFrom", new GridColumnInfo("ΩΡΑ ΑΠΟ", true, fieldNames.Count + 1));
+            fieldNames.Add("DeliveryHourTo", new GridColumnInfo("ΩΡΑ ΕΩΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("PayerType", new GridColumnInfo("ΧΡΕΩΣΗ", true, fieldNames.Count + 1));
+            fieldNames.Add("PackageType", new GridColumnInfo("ΔΕΜΑ/ΦΑΚΕΛΟΣ", false, fieldNames.Count + 1));
+            fieldNames.Add("IsReturn", new GridColumnInfo("ΕΠΙΣΤΡΟΦΗ", true, fieldNames.Count + 1));
+            fieldNames.Add("IsSaturday", new GridColumnInfo("ΣΑΒΒΑΤΟ", true, fieldNames.Count + 1));
+            fieldNames.Add("CostCenter", new GridColumnInfo("ΚΕΝΤΡΟ ΚΟΣΤΟΥΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("Weight", new GridColumnInfo("ΒΑΡΟΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("Count", new GridColumnInfo("ΤΕΜΑΧΙΑ", true, fieldNames.Count + 1));
+            fieldNames.Add("c_Date", new GridColumnInfo("ΗΜ/ΝΙΑ", true, fieldNames.Count + 1));
+            fieldNames.Add("IsPrinted", new GridColumnInfo("ΕΚΤΥΠΩΣΗΣ", true, fieldNames.Count + 1));
+            fieldNames.Add("BillOfLading", new GridColumnInfo("Φορτωτική", true, fieldNames.Count + 1));
+            fieldNames.Add("Memo1", new GridColumnInfo("Σχόλια 1", true, fieldNames.Count + 1));
+            fieldNames.Add("Memo2", new GridColumnInfo("Σχόλια 2", true, fieldNames.Count + 1));
+            fieldNames.Add("TimeCommitment", new GridColumnInfo("Δεσμεύση ώρας", true, fieldNames.Count + 1));
 
-            foreach (var c in columns)
+            int columnCounter = 0;
+            foreach (var c in columns)                
             {
+                c.Visibility = Visibility.Collapsed;
+                columnCounter++;
+                                
                 ContentItemWrapperForColumnHeader headerFeature = (ContentItemWrapperForColumnHeader)c.Header;
                 if (headerFeature != null)
                 {
--- ICourier/ICourier.DesktopClient/Screens/SearchVouchersViews.lsml.cs
+++ ICourier/ICourier.DesktopClient/Screens/SearchVouchersViews.lsml.cs
@@ -7,15 +7,41 @@ using Microsoft.LightSwitch;
 using Microsoft.LightSwitch.Framework.Client;
 using Microsoft.LightSwitch.Presentation;
 using Microsoft.LightSwitch.Presentation.Extensions;
+using System.Windows.Controls;
 namespace LightSwitchApplication
 {
     public partial class SearchVouchersViews
     {
+        private const string _CONTROL = "grid";
+
         partial void SearchVouchersViews_InitializeDataWorkspace(List<IDataService> saveChangesTo)
         {
             DateFrom = DateTime.Today;
             DateTo = DateTime.Today.AddDays(1);
+
+            this.FindControl(_CONTROL).ControlAvailable += grid_ControlAvailable;
             
+        }
+
+        private void grid_ControlAvailable(object sender, ControlAvailableEventArgs e)
+        {
+            DataGrid _dataGrid = (DataGrid)e.Control;
+
+            if (Clients.ClientInfo.CurrentClient == Clients.ClientInfo.Clients.DELATOLAS)
+            {                
+                Clients.DELATOLAS.VoucherGridStyle.CreateStyle(_dataGrid.Columns);
+            }
+            else
+            {
+                var lstNewColumns = new List<string>(new string[] { "Volume", "PayWayCash", "PayWayCheck", "PayWayBillOfExchange",
+                "PayWayDesc", "DeliveryNoteInvoice", "VoucherGroup", "PackType", "Invoice", "MobilePhone", "PayWay", "CostCenterID"});
+                var columns = _dataGrid.Columns.Where(c => lstNewColumns.Contains(c.SortMemberPath));
+                foreach(var col in columns)
+                {
+                    col.Visibility = System.Windows.Visibility.Collapsed;
+                }
+                
+            }
         }
 
         partial void VouchersViews_Loaded(bool succeeded)
